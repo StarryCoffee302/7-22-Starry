7766171d2172e41baef42117b92125433b57f5bf
starrycoffee302 <odeblood632@gmail.com>
2022-02-17 12:26:52 +0700

    git add . git commit -m "my changes"

---
+++ database/migrations/2022_02_10_022348_create_contacs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContacsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('pesan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacs');
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,5 @@
 
 @section('container')
      <h1>Halaman Home</h1>
+     <meta name="csrf-token" content="{{ csrf_token() }}"/>
 @endsection
